b3f7c2362cff796cbd1ba4a8c96bc184a49e5274
d.kuznetsov <mixrus09@gmail.com>
2016-09-28 10:40:19 +0300

    SSL certificate generator update

--- src/SSLCertificateGenerator.php
+++ src/SSLCertificateGenerator.php
@@ -58,19 +58,22 @@ trait SSLCertificateGenerator
             $csrCertificate = openssl_csr_new( $sslDescription, $privateKey );
             $sslCertificate = openssl_csr_sign( $csrCertificate, null, $privateKey, $days );
 
+            $pathToCsr = realpath( $path . DIRECTORY_SEPARATOR . $prefix . 'request.key' );
             openssl_csr_export( $csrCertificate, $csrOutput );
-            file_put_contents( $path . DIRECTORY_SEPARATOR . $prefix . 'request.csr', $csrOutput );
+            file_put_contents( $pathToCsr, $csrOutput );
 
+            $pathToCertificate = realpath( $path . DIRECTORY_SEPARATOR . $prefix . 'certificate.crt' );
             openssl_x509_export( $sslCertificate, $certOutput );
-            file_put_contents( $path . DIRECTORY_SEPARATOR . $prefix . 'certificate.crt', $certOutput );
+            file_put_contents( $pathToCertificate, $certOutput );
 
+            $pathToPrivateKey = realpath( $path . DIRECTORY_SEPARATOR . $prefix . 'privatekey.key' );
             openssl_pkey_export( $privateKey, $pkeyOutput );
-            file_put_contents( $path . DIRECTORY_SEPARATOR . $prefix . 'privatekey.key', $pkeyOutput );
+            file_put_contents( $pathToPrivateKey, $pkeyOutput );
 
             return [
-                'requestPath'     => $path . DIRECTORY_SEPARATOR . 'request.csr',
-                'certificatePath' => $path . DIRECTORY_SEPARATOR . 'certificate.crt',
-                'privatekeyPath'  => $path . DIRECTORY_SEPARATOR . 'privatekey.key',
+                'csrPath'         => $pathToCsr,
+                'certificatePath' => $pathToCertificate,
+                'privatekeyPath'  => $pathToPrivateKey,
             ];
         } else {
             throw new \Exception( 'Can not write in folder ' . $path );
