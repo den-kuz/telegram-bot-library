17b9db140963e651c1281ffa5552161cd6c88296
d.kuznetsov <mixrus09@gmail.com>
2016-10-03 10:12:13 +0300

    realpath fix

--- src/FileSystemHelper.php
+++ src/FileSystemHelper.php
@@ -12,10 +12,8 @@ use TelegramBotLibrary\Exceptions\TelegramException;
 
 trait FileSystemHelper
 {
-    public static function saveContent ( $content, $saveDir = './', $saveFilename = null, $saveFileExtension = null )
+    public static function saveContent ( $content, $saveDir = __DIR__, $saveFilename = null, $saveFileExtension = null )
     {
-        $saveDir = realpath( $saveDir );
-
         if ( !is_dir( $saveDir ) ) @mkdir( $saveDir, true );
         if ( !is_dir( $saveDir ) || !is_writeable( $saveDir ) )
             throw new TelegramException( 'Directory "' . $saveDir . '"  path is not writable' );
@@ -25,12 +23,11 @@ trait FileSystemHelper
         $fullFilePath .= !is_null( $saveFilename ) ? $saveFilename : md5( md5( $content ) . md5( microtime() ) );
         $fullFilePath .= !is_null( $saveFileExtension ) ? '.' . $saveFileExtension : '';
 
-        $fullFilePath = realpath( $fullFilePath );
         $writeResult = file_put_contents( $fullFilePath, $content );
         if ( $writeResult ) {
             throw new TelegramException( 'Can not write in file "' . $fullFilePath . '"' );
         }
 
-        return $fullFilePath;
+        return realpath( $fullFilePath );
     }
 }
